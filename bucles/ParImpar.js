8a46ed4564a1b182af0f639307abf9b762c8ece9
Juanka <62060237+icyjkk@users.noreply.github.com>
2022-11-03 20:38:33 +0100

    Add files via upload

---
+++ bucles/ParImpar.js
@@ -0,0 +1,9 @@
+
+for(var i=0;i<=20;i++){
+
+  if(i%2==0){
+    console.log(i+" es par");
+  }else{
+    console.log(i+" es impar");
+  }
+}
---
+++ bucles/TablasDeMultiplicar.js
@@ -0,0 +1,12 @@
+
+for(var i=1;i<=10;i++){
+
+  console.log("Tabla del "+i+":");
+  
+  for(var j=1;j<11;j++){
+    sol=i*j;
+    console.log(i+"*"+j+"="+sol);
+  }
+  console.log(" ");
+  
+}
